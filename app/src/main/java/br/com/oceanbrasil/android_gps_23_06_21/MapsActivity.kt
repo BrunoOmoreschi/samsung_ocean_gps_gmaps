8f58458d80fa0fc76763b2e733548066741ce536
BrunoOmoreschi <brunomoreschi.camposdesouza@gmail.com>
2021-07-05 21:40:51 -0300

    App completo de estudo
    O app foi completado com implementação do GPS e das permissões, local base caso não tenha como contatar a antena e pedidos de permissão.

---
+++ app/src/main/java/br/com/oceanbrasil/android_gps_23_06_21/MapsActivity.kt
@@ -0,0 +1,89 @@
+package com.oceanbrasil.android_gps_23_06_2021
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import br.com.oceanbrasil.android_gps_23_06_21.R
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+const val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        // Add a marker in Sydney and move the camera
+        val saoPaulo = LatLng(-23.59284516630147, -46.67206405699086)
+        mMap.addMarker(MarkerOptions().position(saoPaulo).title("Marca em São Paulo"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(saoPaulo, 18f))
+
+        iniciarLocalizacao()
+    }
+
+    private fun iniciarLocalizacao() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), LOCATION_PERMISSION_REQUEST_CODE
+            )
+
+            return
+        }
+
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationProvider = LocationManager.GPS_PROVIDER
+
+        val ultimaLocalizacao = locationManager.getLastKnownLocation(locationProvider)
+
+        Toast.makeText(
+            this,
+            "Lat: ${ultimaLocalizacao?.latitude} Lng: ${ultimaLocalizacao?.longitude}",
+            Toast.LENGTH_LONG
+        ).show()
+
+    }
+}
